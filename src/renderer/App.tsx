7f7635b82f32e80450b8ea915f8af6d0c4499bf3
王金海(haha.wang) <wanghaha@huolala.cn>
2021-08-13 15:50:01 +0800

    firstcommit

---
+++ src/renderer/App.tsx
@@ -0,0 +1,31 @@
+import type * as React from 'react'
+import { motion } from 'framer-motion'
+import TopBar from './components/top-bar'
+import logo from './logo.png'
+
+const containerMotion = {
+  initial: 'hidden',
+  animate: 'visible',
+  variants: {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2
+      }
+    }
+  }
+}
+
+function App() {
+  return (
+    <div tw="h-screen w-screen flex flex-row">
+      <TopBar />
+      <motion.div tw="h-full w-full bg-white" {...containerMotion}></motion.div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/renderer/components/top-bar.tsx
@@ -0,0 +1,9 @@
+export default function TopBar() {
+  return (
+    <nav
+      tw="h-screen w-1/4 bg-black"
+      className="webkit-drag"
+      style={{ minWidth: '200px' }}
+    ></nav>
+  )
+}
